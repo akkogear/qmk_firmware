6f1cb6a32ecc7553af75d35ac4aaaf061dba1e6c
yangzheng <1345509173@q.com>
2025-01-17 09:21:05 +0800

    add 5075b keyboard

---
+++ keyboards/akko/5075b/iso/post_rules.mk
@@ -0,0 +1,6 @@
+RGB_MATRIX_CUSTOM_USER = yes
+RGBLIGHT_DRIVER = custom
+include keyboards/akko/5075b/iso/rgb_record/rgb_record.mk
+include keyboards/akko/5075b/iso/wls/wls.mk
+include keyboards/linker/wireless/wireless.mk
+
